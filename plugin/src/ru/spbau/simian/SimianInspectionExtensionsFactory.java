d6a99ab0aab6be1c35ca5a2d2b25e7b20cb451d7
Maria Fofanova <fofanova.mn@gmail.com>
2012-04-08 22:52:19 +0400

    plugin init

---
+++ plugin/src/ru/spbau/simian/SimianInspectionExtensionsFactory.java
@@ -0,0 +1,46 @@
+package ru.spbau.simian;
+
+import com.intellij.codeInspection.HTMLComposer;
+import com.intellij.codeInspection.ex.GlobalJavaInspectionContextImpl;
+import com.intellij.codeInspection.lang.GlobalInspectionContextExtension;
+import com.intellij.codeInspection.lang.HTMLComposerExtension;
+import com.intellij.codeInspection.lang.InspectionExtensionsFactory;
+import com.intellij.codeInspection.lang.RefManagerExtension;
+import com.intellij.codeInspection.reference.RefManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+
+/**
+ * @author: maria
+ */
+public class SimianInspectionExtensionsFactory extends InspectionExtensionsFactory {
+    @Override
+    public GlobalInspectionContextExtension createGlobalInspectionContextExtension() {
+        return null;
+    }
+
+    @Override
+    public RefManagerExtension createRefManagerExtension(RefManager refManager) {
+        return null;
+    }
+
+    @Override
+    public HTMLComposerExtension createHTMLComposerExtension(HTMLComposer composer) {
+        return null;
+    }
+
+    @Override
+    public boolean isToCheckMember(PsiElement element, String id) {
+        return false;
+    }
+
+    @Override
+    public String getSuppressedInspectionIdsIn(PsiElement element) {
+        return null;
+    }
+
+    @Override
+    public boolean isProjectConfiguredToRunInspections(Project project, boolean online) {
+        return GlobalJavaInspectionContextImpl.isInspectionsEnabled(online, project);
+    }
+}
