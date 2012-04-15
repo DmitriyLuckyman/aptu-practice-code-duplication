348c4dea6f4e7b10b11fead5f381e53392f80273
Maria Fofanova <fofanova.mn@gmail.com>
2012-04-16 01:39:29 +0400

    add parameters like a boss

--- plugin/src/ru/spbau/duplication/DuplicateQuickFix.java
+++ plugin/src/ru/spbau/duplication/DuplicateQuickFix.java
@@ -1,15 +1,15 @@
 package ru.spbau.duplication;
 
-import com.intellij.codeInsight.completion.JavaCompletionUtil;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
-import com.intellij.psi.impl.source.codeStyle.ImportHelper;
 import com.intellij.refactoring.util.duplicates.Match;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * @author: maria
@@ -40,13 +40,26 @@ public class DuplicateQuickFix implements LocalQuickFix {
         final PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
         final PsiClass containingClass = method.getContainingClass();
         assert containingClass != null;
-        final String text = containingClass.getName() + "." + method.getName() + "()";
-        PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) factory.createExpressionFromText(text, null);
+        final StringBuilder callText = new StringBuilder();
+        callText.append(containingClass.getName()).append(".").append(method.getName()).append("(");
+        int i = 0;
+        for (PsiVariable parameter : method.getParameterList().getParameters()) {
+            if (i > 0) {
+                callText.append(", ");
+            }
+            List<PsiElement> parameterValues = match.getParameterValues(parameter);
+            callText.append(parameterValues == null || parameterValues.isEmpty() ?
+                    "null" :
+                    parameterValues.get(0).getText());
+            ++i;
+        }
+        callText.append(")");
+        PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) factory.createExpressionFromText(callText.toString(), null);
         methodCallExpression = (PsiMethodCallExpression) CodeStyleManager.getInstance(method.getManager()).reformat(methodCallExpression);
 
         final PsiFile matchFile = match.getFile();
         match.replace(method, methodCallExpression, null);
-        if(methodCallExpression.getMethodExpression().resolve() == null){
+        if (methodCallExpression.getMethodExpression().resolve() == null) {
             final PsiClass aClass = method.getContainingClass();
             assert aClass != null;
             JavaCodeStyleManager.getInstance(project).addImport((PsiJavaFile) matchFile, aClass);
